931bfa5b9a4c77311091afc046a0de437d759d32
Marcelo Ruiz - mbp <mackciber@gmail.com>
2025-07-05 11:51:30 -0400

    add languaje

---
+++ lua/config/exec_languaje.lua
@@ -0,0 +1,37 @@
+local Terminal = require("toggleterm.terminal").Terminal
+
+vim.keymap.set("n", "<F5>", function()
+    vim.cmd("w")                          -- Guarda el archivo
+    local ft = vim.bo.filetype
+    local filepath = vim.fn.expand("%:t") -- Ruta completa del archivo
+    local output = vim.fn.expand("%:r")   -- Ruta del ejecutable (sin .c)
+
+    local cmd = ""
+    if ft == "c" then
+        cmd = string.format("gcc \"%s\" -o \"%s\" && \"%s\"", filepath, output, output)
+    elseif ft == "cpp" then
+        cmd = string.format("g++ \"%s\" -o \"%s\" && \"%s\"", filepath, output, output)
+    elseif ft == "java" then
+        cmd = string.format("javac \"%s\" && java -cp \"%s\" %s", filepath, vim.fn.expand("%:p:h"), basename)
+    elseif ft == "go" then
+        cmd = string.format("go run \"%s\"", filepath)
+    elseif ft == "python" then
+        cmd = string.format("python3 \"%s\"", filepath)
+    elseif ft == "ruby" then
+        cmd = string.format("ruby \"%s\"", filepath)
+    elseif ft == "sh" then
+        cmd = string.format("bash \"%s\"", filepath)
+    elseif ft == "lua" then
+        cmd = string.format("lua \"%s\"", filepath)
+    else
+        vim.notify("Language not soported")
+    end
+    local term = Terminal:new({
+        -- cmd = full_cmd,
+        cmd = cmd,
+        direction = "float",
+        close_on_exit = false,
+        hidden = true
+    })
+    term:toggle()
+end, { noremap = true, silent = true })
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,34 +1,24 @@
---[[ local function enable_transparency()
+local function enable_transparency()
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
-end ]]
+end
 return {
     {
         "folke/tokyonight.nvim",
         lazy = false,
         priority = 1000,
         config = function()
-            -- vim.cmd("colorscheme tokyonight")
-            -- enable_transparency()
+            vim.cmd("colorscheme tokyonight")
+            enable_transparency()
         end
     },
     {
         'sainnhe/gruvbox-material',
         config = function()
-            vim.cmd("colorscheme gruvbox-material")
+            -- vim.cmd("colorscheme gruvbox-material")
             vim.g.gruvbox_material_background = "hard"
             vim.g.lightline = {}
-            -- enable_transparency()
-        end
-    },
-    {
-        "Mofiqul/vscode.nvim",
-        name = 'vscode',
-        config = function()
-            -- vim.cmd("colorscheme vscode")
-            vim.cmd('hi Directory guibg=NONE')
-            vim.cmd('hi SignColumn guibg=NONE')
             -- enable_transparency()
         end
     },
