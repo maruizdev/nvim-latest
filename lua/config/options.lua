5ae0080b32950f6df487475353d649b7ed1dc336
Marcelo Ruiz <marcelo@mruiz.autoridadminera.gob.bo>
2025-07-16 20:51:51 -0400

    add

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -54,6 +54,5 @@ set.incsearch = true
 -- faster cursor hold
 set.updatetime = 50
 
--- vim.cmd("colorscheme gruvbox-material")
 -- vim.cmd("colorscheme vscode")
 -- compile gcc
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,15 +5,6 @@
 end ]]
 return {
     {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            -- vim.cmd("colorscheme tokyonight-moon")
-            -- enable_transparency()
-        end
-    },
-    {
         'sainnhe/gruvbox-material',
         config = function()
             -- vim.cmd("colorscheme gruvbox-material")
@@ -21,18 +12,6 @@ return {
             vim.g.lightline = {}
             -- enable_transparency()
         end
-    },
-    {
-        'projekt0n/github-nvim-theme',
-        name = 'github-theme',
-        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            require('github-theme').setup({
-            })
-
-            vim.cmd('colorscheme github_dark')
-        end,
     },
     {
         "nvim-lualine/lualine.nvim",
