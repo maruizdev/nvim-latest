bcab3d42703acfd822e400fa43b19de59eba7877
Marcelo Ruiz <marcelo@mruiz.autoridadminera.gob.bo>
2025-07-24 11:26:57 -0400

    add colorscheme onedark

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,7 +4,18 @@ return {
         config = function()
             vim.g.gruvbox_material_background = "hard"
             vim.g.lightline = {}
-            vim.cmd("colorscheme gruvbox-material")
+            -- vim.cmd("colorscheme gruvbox-material")
         end
     },
+    {
+        "navarasu/onedark.nvim",
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            require('onedark').setup {
+                style = 'darker'
+            }
+            -- Enable theme
+            require('onedark').load()
+        end
+    }
 }
